7d45e66d4ea2fc17f4db658ae3afdea2272f74b6
dmanaligod96 <114620107+dmanaligod96@users.noreply.github.com>
2024-12-06 02:41:25 -0600

    New assets and changes to scripts

--- Assets/Assets/Resources/Scripts/CheckpointTrigger.cs
+++ Assets/Assets/Resources/Scripts/CheckpointTrigger.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class CheckpointTrigger : MonoBehaviour
 {
-    [SerializeField] string winSceneName = "GameOverWinScene";
+    [SerializeField] private Sprite checkpointActiveSprite; // The sprite to display when checkpoint is active
+    [SerializeField] private Sprite checkpointDefaultSprite; // The default sprite before the checkpoint is activated
+    [SerializeField] static bool checkpointSet = false;
+    private static Vector3 checkpointPosition;
+    
 
-    private void OnTriggerEnter2D(Collider2D collision){
+    private SpriteRenderer spriteRenderer;
 
-        if(collision.gameObject.CompareTag("Player")){
-            LoadWinScene();
+    private void Start()
+    {
+        // Get the SpriteRenderer component
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // Set the default sprite initially
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sprite = checkpointDefaultSprite;
         }
-
     }
-    private void LoadWinScene(){
-        SceneManager.LoadScene(winSceneName);
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Set checkpoint position when the player hits the checkpoint
+            checkpointPosition = transform.position;
+            checkpointSet = true;
+
+            // Change the sprite when the checkpoint is set
+            if (spriteRenderer != null && checkpointSet)
+            {
+                spriteRenderer.sprite = checkpointActiveSprite;
+            }
+        }
+    }
+
+    public static void RespawnPlayer(GameObject player)
+    {
+        if (checkpointSet)
+        {
+            player.transform.position = checkpointPosition;
+        }
     }
 }
