2a141049c932e8c6aa4b98096aa9cd402c164867
dmanaligod96 <114620107+dmanaligod96@users.noreply.github.com>
2024-12-06 04:37:22 -0600

    Changed main menu MainMenuManager instead of demo moved to tutorial

--- Assets/Assets/Resources/Scripts/MainMenuManager.cs
+++ Assets/Assets/Resources/Scripts/MainMenuManager.cs
@@ -7,7 +7,7 @@ public class MainMenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public void StartGame(){
-        SceneManager.LoadScene("DemoGame");
+        SceneManager.LoadScene("Tutorial");
 
     }
 
